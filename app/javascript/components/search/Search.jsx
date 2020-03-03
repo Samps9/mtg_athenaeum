9cbc2d2434050d55278657d1f4259844179261d2
Sam Lubin <samlubin@yahoo.com>
2020-03-03 12:25:28 -0500

    clear results button

--- app/javascript/components/search/Search.jsx
+++ app/javascript/components/search/Search.jsx
@@ -16,6 +16,7 @@ class Search extends React.Component {
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.clearResults = this.clearResults.bind(this);
   }
 
   // Results 'Quality Of Life' (or 'Length' perhaps more aptly) - shorthand because this logic gets used a lot
@@ -51,6 +52,10 @@ class Search extends React.Component {
     );
   }
 
+  clearResults() {
+    this.setState({searchResults: [], isLoading: false, resultsFound: false, searchTerm: ''})
+  }
+
   componentDidUpdate() {
     // reset the state of isLoading to stop loading animation after results populate
     if (this.state.isLoading && this.resultsQOL()) {
@@ -59,12 +64,21 @@ class Search extends React.Component {
     // if there are actual cards in search results, set resultsFound to true
     if (this.resultsQOL() && this.state.searchResults[0] !== "No results found" && !this.state.resultsFound) {
       this.setState({resultsFound: true});
-    } 
+    }
   }
 
   render() {
 
     const greeting = <Greeting />;
+
+    const clearButton = (
+      <div className="col-2">
+        <button onClick={this.clearResults}className="btn btn-outline-dark ml-2 mr-2 form-control" type="reset">
+          <i className="fa fa-ban"></i>
+        </button>
+      </div>
+    );
+     
 
     const spinner = <Spinner  searchTerm = {this.state.searchTerm} />;
     
@@ -75,12 +89,15 @@ class Search extends React.Component {
     );
 
     const noResults = <NoResults emptySearchTerm = {this.state.emptySearchTerm} />;
+
     return (
       <>
         <div className="search-container">
+          
           <form onSubmit={this.handleSubmit} className="my-2" autoComplete="off">
             <div className="form-row">
-              <div className="col-9">
+              {this.resultsQOL() && this.state.resultsFound ? clearButton : null}
+              <div className="col-8">
                 <input value={this.state.searchTerm} onChange={this.handleChange} className="form-control ml-2 mr-2" type="text" placeholder="Search for cards by name" aria-label="Search" />
               </div>
               <div className="col-2">
@@ -88,7 +105,6 @@ class Search extends React.Component {
                   <i className="fa fa-search"></i>
                 </button>
               </div>
-              
             </div>
           </form>
           
@@ -96,13 +112,13 @@ class Search extends React.Component {
           {(
             () => {
               if(this.resultsQOL() && this.state.resultsFound) {
-                return results
+                return results;
               } else if(this.state.isLoading) {
-                return spinner
+                return spinner;
               } else if(this.resultsQOL() && this.state.searchResults[0] === "No results found") {
-                return noResults
+                return noResults;
               } else {
-                return greeting
+                return greeting;
               }
             }
           )()}
